f79e625ad9497401cc5e7ba898993a5ba6338ac6
Christopher Taylor <134958907+cmtayl0r@users.noreply.github.com>
2025-05-10 15:17:07 +0200

    Remove unused ToastHost import from App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { ModalProvider } from "./context/ModalContext";
 import { ToastProvider } from "./context/ToastContext";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
-import ToastHost from "./components/ui/toast/ToastHost";
 import { StacksProvider } from "./context/StacksContext";
 import { ActionsProvider } from "./context/ActionsContext";
 
