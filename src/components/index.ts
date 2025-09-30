4b6c517bb2c58cbc91ed2dfdebbb528fea0882da
Christopher Taylor <134958907+cmtayl0r@users.noreply.github.com>
2025-09-30 08:54:35 +0200

    refactor: Update component exports for improved clarity and organization; streamline UI and layout exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,15 +9,14 @@ UI
 ================================================================================
 */
 
-export {
-  BaseModal,
-  ModalHost,
-  Toast,
-  ToastContainer,
-  LoadingSpinner,
-  Button,
-} from "./ui";
-// export type { ModalProps } from "./ui";
+export { default as BaseModal } from "./ui/modal/BaseModal";
+export { ModalHost } from "./ui/modal/ModalHost";
+export { MODAL_IDS } from "./ui/modal/ModalHost";
+export { default as Button } from "./ui/button/Button";
+export { Toast, ToastContainer } from "./ui/toast/Toast";
+export { default as LoadingSpinner } from "./ui/loading-spinner/LoadingSpinner";
+export { default as ThemeSelector } from "./ui/theme-selector/ThemeSelector";
+export type { ButtonProps } from "./ui/button/Button";
 
 /*
 ================================================================================
@@ -25,10 +24,26 @@ LAYOUT
 ================================================================================
 */
 
-// export { Sidebar, AppLayout } from './layout';
+export { default as AppLayout } from "./layout/app-layout/AppLayout";
+export { default as Sidebar } from "./layout/sidebar/Sidebar";
+export { default as Header } from "./layout/header/Header";
+export { default as StackView } from "./layout/stack-view/StackView";
+export { default as ErrorView } from "./layout/error-view/ErrorView";
 
 /*
 ================================================================================
 FEATURES
 ================================================================================
 */
+// Actions
+export { default as ActionListItem } from "./features/actions/ActionListItem";
+export { default as AddActionModal } from "./features/actions/AddActionModal";
+// export { default as EditActionFoundation } from "./features/actions/EditAction-foundation";
+
+// Search
+export { default as SearchActionsModal } from "./features/search/SearchActionsModal";
+
+// Stacks
+export { default as StackFilter } from "./features/stacks/StackFilter";
+export { default as StackList } from "./features/stacks/StackList";
+export { default as AddStackModal } from "./features/stacks/AddStackModal";
